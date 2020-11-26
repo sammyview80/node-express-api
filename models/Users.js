709085e4cdabdad83db3dd80e3929f9fc6d21420
sammyview80 <sammyview80@gmail.com>
2020-11-26 09:21:02 +0545

    make it better

--- models/Users.js
+++ models/Users.js
@@ -1,9 +1,9 @@
 const crypto = require('crypto');
-const mogoose = require('mongoose');
+const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const UserSchema = new mogoose.Schema({
+const UserSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Please add a name']
